6694198321d2e922a878ea243595a08a2d16ffc4
Manatuuu <37593138+Manatuuu@users.noreply.github.com>
2019-07-21 23:37:33 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,28 @@
 <?php 
-    if (($sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)) === false) {
-        die("socket_create() failed: ".socket_strerror(socket_last_error()));
+  $url = 'tcp://59.136.95.107:25565';
+  $port = 25565;
+  $timeout = 30;
+
+  $fp = fsockopen( $url, $port, &$errno, &$errstr, $timeout );
+  if( !$fp || $errno > 0 ) {
+    print( "$errno ($errstr) \n" );
+    exit();
+  }
+
+  while( true ) {
+    print( 'input>' );
+    // scanf関数は下のほう参照。
+    $input = scanf();
+    $send_text = sprintf( "%s %s\n", date( 'YmdHis' ), $input );
+
+    print( 'SEND>>' . $send_text );
+    fwrite( $fp, $send_text );
+    print( 'RECV<<' . fgets( $fp, 4096 ) );
+
+    if( $input == 'exit' ) {
+      break;
     }
-
-    if (socket_connect($sock, '59.136.95.107', 25565) === false) {
-        die("socket_connect() failed: ".socket_strerror(socket_last_error()));
-    }
-
-    $buf = "NICK:Test\n";
-    socket_write($sock, $buf, strlen($buf));
-    echo socket_read($sock, strlen($buf));
-
-    $buf = "Hello\n";
-    socket_write($sock, $buf, strlen($buf));
-    echo socket_read($sock, strlen($buf));
+  }
+  fclose( $fp );
+  exit();
 ?>
