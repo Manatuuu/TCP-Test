c63fd21802f91ddafbfa74808ceb522bbd449241
Manatuuu <37593138+Manatuuu@users.noreply.github.com>
2019-07-21 20:27:14 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php 
-echo "Hello";
-echo "Hello";
-echo "Hello";
-echo "Hello";
+if (($sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)) === false) {
+    die("socket_create() failed: ".socket_strerror(socket_last_error()));
+}
+
+if (socket_connect($sock, '59.136.95.107', 25565) === false) {
+    die("socket_connect() failed: ".socket_strerror(socket_last_error()));
+}
+
+$buf = "foo\n";
+socket_write($sock, $buf, strlen($buf));
+echo socket_read($sock, strlen($buf));
+
+$buf = "bar\n";
+socket_write($sock, $buf, strlen($buf));
+echo socket_read($sock, strlen($buf));
 ?>
