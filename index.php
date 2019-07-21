da310e9c1c6c9554d2a4fc51176d78290919e923
Manatuuu <37593138+Manatuuu@users.noreply.github.com>
2019-07-21 23:33:47 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,17 @@
 <?php 
-    $destination = "tcp://59.136.95.107:25565";
-    $socket = stream_socket_client($destination);
-
-    if ($socket ===false) {
-        print("ソケットの確立に失敗");
-        exit();
-    } else {
-        // ソケットへの接続時お名前を入力する
-        $yourName = readline(">>> Input your name ");
-        while (true) {
-            print("{$yourName}さん");
-            $input = readline(">>> ");
-            $sendingMessage = "{$yourName}:{$input}";
-            fwrite($socket, $sendingMessage, strlen($sendingMessage));
-            stream_set_timeout($socket, 1);
-            // サーバー側からのレスポンスを取得
-            while(true) {
-                $read = fread($socket, 1024);
-                print($read.PHP_EOL);
-                if (strlen($read) === 0) {
-                    break;
-                }
-            }
-        }
+    if (($sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)) === false) {
+        die("socket_create() failed: ".socket_strerror(socket_last_error()));
     }
+
+    if (socket_connect($sock, '127.0.0.1', 5000) === false) {
+        die("socket_connect() failed: ".socket_strerror(socket_last_error()));
+    }
+
+    $buf = "NICK:Test\n";
+    socket_write($sock, $buf, strlen($buf));
+    echo socket_read($sock, strlen($buf));
+
+    $buf = "Hello\n";
+    socket_write($sock, $buf, strlen($buf));
+    echo socket_read($sock, strlen($buf));
 ?>
