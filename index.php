2c25219856113ff880aecdae13b55f7d96c07512
Manatuuu <37593138+Manatuuu@users.noreply.github.com>
2019-07-21 23:35:08 +0900

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
         die("socket_create() failed: ".socket_strerror(socket_last_error()));
     }
 
-    if (socket_connect($sock, '127.0.0.1', 5000) === false) {
+    if (socket_connect($sock, '59.136.95.107', 25565) === false) {
         die("socket_connect() failed: ".socket_strerror(socket_last_error()));
     }
 
